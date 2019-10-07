e14b738d018b2292f83c7bc77ab070bed3760b6c
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-07 17:52:23 +0300

    Add dialog and notification logic

--- src/User Experience/dialogsAndNotifications/script.js
+++ src/User Experience/dialogsAndNotifications/script.js
@@ -1,4 +1,10 @@
 const init = () => {
+  const REDIRECT_URL = 'http://dmitrywhite.com';
+  const NOTIFICATION_ICON_PATH = 'info.png';
+  const PERMISSION_STATUS_GRANTED = 'granted';
+  const DIALOG_STATUS_OK = 'Ok';
+  const DIALOG_STATUS_CANCEL = 'Cancel';
+
   const buttonNonModal = document.querySelector('#show1');
   const buttonModal = document.querySelector('#show2');
   const buttonOk = document.querySelector('#okBtn');
@@ -12,40 +18,31 @@ const init = () => {
   const notificationIcon = document.querySelector('#notIcon');
   const notificationPersist = document.querySelector('#notPersist');
 
+  const isPermissionGranted = () => Notification.permission === PERMISSION_STATUS_GRANTED;
+  const redirectHandler = (url) => (event) => {
+    event.target.close();
+    window.location = url;
+  };
+
   // event handlers to show the dialog as non-modal and modal
-  buttonNonModal.addEventListener('click', () => {
-    // TODO: Show the dialog using the non-modal API
-
-  });
-  buttonModal.addEventListener('click', () => {
-    // TODO: Show the dialog using the modal API
-
-  });
+  buttonNonModal.addEventListener('click', () => dialog.show());
+  buttonModal.addEventListener('click', () => dialog.showModal());
 
   // add event listeners for the OK and Cancel buttons
-  buttonOk.addEventListener('click', () => {
-    // TODO: Check to see if the dialog is in fact open, if so then close it with "OK"
-  });
-  buttonCancel.addEventListener('click', () => {
-    // TODO: Check to see if the dialog is in fact open, if so then close it with "Cancel"
-  });
+  buttonOk.addEventListener('click', () => dialog.open && dialog.close(DIALOG_STATUS_OK));
+  buttonCancel.addEventListener('click', () => dialog.open && dialog.close(DIALOG_STATUS_CANCEL));
 
   // event listeners for the dialog itself - close and cancel
   dialog.addEventListener('close', () => console.log('Dialog closed: ', dialog.returnValue));
   dialog.addEventListener('cancel', () => console.log('Dialog canceled: ', dialog.returnValue));
 
-  buttonRequest.addEventListener('click', () => {
-    // TODO: Request notifications permission from the user
+  buttonRequest.addEventListener('click', () => Notification.requestPermission()
+    .then((res) => console.log('Permission granted: ', res)));
 
-  });
-
-  buttonShowBasic.addEventListener('click', () => {
-    // TODO: check to make sure we have permission to show notifications
-
-  });
+  buttonShowBasic.addEventListener('click', () => isPermissionGranted() && new Notification('Basic notification'));
 
   buttonShowCustom.addEventListener('click', () => {
-    if (Notification.permission === 'granted') {
+    if (isPermissionGranted()) {
       // Get the options settings
       const title = notificationTitle.value;
       const body = notificationBody.value;
@@ -53,12 +50,17 @@ const init = () => {
       const isPersistent = notificationPersist.checked;
 
       // TODO: Set up the notification options
-
+      const options = {
+        body,
+        requireInteraction: isPersistent,
+        icon: useIcon && NOTIFICATION_ICON_PATH,
+      };
 
       // TODO: Show the notification
-
+      const customNotification = new Notification(title, options);
 
       // TODO: Handle a click on the notification to open a new page
+      customNotification.addEventListener('click', redirectHandler(REDIRECT_URL));
     }
   });
 };
