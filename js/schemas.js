376576b77464f5796ac6faf4b3d5518e5cf90beb
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-13 17:57:03 +0300

    Add products schema

---
+++ js/schemas.js
@@ -0,0 +1,27 @@
+const productSchema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "id": "http://example.com/schemas/products.json",
+  "title": "H+ sport products",
+  "description": "Schema for H+ sport product data",
+
+  "type:": "object",
+  "properties": {
+    "products": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "image": {
+            "type": "string"
+          },
+          "alt": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+};
