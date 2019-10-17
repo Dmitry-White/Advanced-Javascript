dd1d79ad273bb01bd14ccb39bbf11495d69b4b66
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-17 15:58:57 +0300

    Add refactored beacon API logic

---
+++ src/Performance/beaconApi/script.js
@@ -0,0 +1,20 @@
+(() => {
+  const beaconURL = 'https://putsreq.com/o68jJ35Is0GdNuhiVnMc';
+
+  const LOAD_MESSAGE = 'loadEvent';
+  const UNLOAD_MESSAGE = 'unloadEvent';
+  const BUTTON_CLICK_MESSAGE_1 = 'Button 1 click';
+  const BUTTON_CLICK_MESSAGE_2 = 'Button 2 click';
+
+  const buttonOne = document.querySelector('#btn1');
+  const buttonTwo = document.querySelector('#btn2');
+
+  const sendEvent = (strEvent) => navigator.sendBeacon && navigator.sendBeacon(beaconURL, strEvent);
+
+  window.addEventListener('load', () => sendEvent(LOAD_MESSAGE));
+
+  buttonOne.addEventListener('click', () => sendEvent(BUTTON_CLICK_MESSAGE_1));
+  buttonTwo.addEventListener('click', () => sendEvent(BUTTON_CLICK_MESSAGE_2));
+
+  window.addEventListener('unload', () => sendEvent(UNLOAD_MESSAGE));
+})();
