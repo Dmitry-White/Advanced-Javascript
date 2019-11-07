83070ce34189fc12e8abd06fc4ea328f0d10bc22
Dmitry <ecio1476@gmail.com>
2019-11-07 19:20:45 +0300

    Explore var-let-const differences

--- scopes/app.js
+++ scopes/app.js
@@ -1,1 +1,12 @@
 console.log('Hello Scopes!');
+
+var warrior = 'Ninja';
+
+const screamWarrior = () => {
+    let warrior2 = 'Samurai';
+    console.log(warrior, warrior2);
+}
+
+screamWarrior();
+
+console.log(warrior, warrior2);
