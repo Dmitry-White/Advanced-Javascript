0560b910162637750814cb3e698947d249c1aad4
Dmitry <ecio1476@gmail.com>
2019-11-09 20:00:51 +0300

    Remove redundant code

--- scopes/app.js
+++ scopes/app.js
@@ -1,27 +1,3 @@
-
-console.log('Hello Scopes!');
-
-const warrior = {
-  name: 'Jujin Take',
-  type: 'Ninja',
-  weapon: 'Shuriken',
-  agility: 79,
-};
-let warrior3;
-
-const screamWarrior = () => {
-  const warrior2 = 'Samurai';
-  warrior3 = 'Viking';
-  return {
-    shootWarrior: () => console.log(warrior, warrior2),
-  };
-};
-
-const newWarrior = screamWarrior();
-newWarrior.shootWarrior();
-
-console.log(warrior);
-
 const warriors = [
   {
     name: 'Jujin Take',
