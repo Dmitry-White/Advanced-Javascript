c23bf6880fbda43b82ef2040a510ab8d23602464
Dmitry <ecio1476@gmail.com>
2019-11-09 19:59:03 +0300

    Add warriors array

--- scopes/app.js
+++ scopes/app.js
@@ -1,22 +1,48 @@
-'use strict'
+
 console.log('Hello Scopes!');
 
 const warrior = {
-    name: 'Jujin Take',
-    type: 'Ninja',
-    weapon: 'Shuriken',
-    agility: 79,
+  name: 'Jujin Take',
+  type: 'Ninja',
+  weapon: 'Shuriken',
+  agility: 79,
 };
+let warrior3;
 
 const screamWarrior = () => {
-    const warrior2 = 'Samurai';
-    warrior3 = 'Viking';
-    return {
-        shootWarrior: () => console.log(warrior, warrior2),
-    };
+  const warrior2 = 'Samurai';
+  warrior3 = 'Viking';
+  return {
+    shootWarrior: () => console.log(warrior, warrior2),
+  };
 };
 
 const newWarrior = screamWarrior();
 newWarrior.shootWarrior();
 
-console.log(warrior, warrior3);
+console.log(warrior);
+
+const warriors = [
+  {
+    name: 'Jujin Take',
+    type: 'Ninja',
+    weapon: 'Shuriken',
+    agility: 79,
+  },
+  {
+    name: 'Viggo Rusen',
+    type: 'Viking',
+    weapon: 'Swing blade',
+    agility: 60,
+  },
+  {
+    name: 'Ro Rake',
+    type: 'Samurai',
+    weapon: 'Boken',
+    agility: 82,
+  },
+];
+
+const listWarriors = () => console.log(warriors);
+
+listWarriors();
