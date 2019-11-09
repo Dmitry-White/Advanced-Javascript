73612cda3404468dbc564cdb1f23b69d93be9dc7
Dmitry <ecio1476@gmail.com>
2019-11-09 20:11:51 +0300

    Add platoon magic

--- scopes/app.js
+++ scopes/app.js
@@ -1,18 +1,9 @@
-const screamWarrior1 = () => {
-  const warrior1 = 'Ninja';
-  const warrior2 = 'Samurai';
+const numberOfPlatoon = 45;
 
-  return `Our warriors are ${warrior1} and ${warrior2}`;
+const platoon = () => {
+  const warriorsPerPlatoon = 60;
+  const totalWarriors = numberOfPlatoon * warriorsPerPlatoon;
+  return `Our army is ${totalWarriors} strong!`;
 };
 
-const screamWarrior2 = () => {
-  const warrior1 = 'Viking';
-  const warrior2 = 'Soldier';
-
-  return `Our warriors are ${warrior1} and ${warrior2}`;
-};
-
-console.log(screamWarrior1());
-console.log(screamWarrior2());
-
-console.log(warrior1, warrior2);
+console.log(platoon());
