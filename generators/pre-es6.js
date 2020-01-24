124239e2c2703748614202117d5d241f0bf41850
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-24 21:06:17 +0300

    Add iterator returning function and marked the states

--- generators/pre-es6.js
+++ generators/pre-es6.js
@@ -1,9 +1,19 @@
+// --------------- ES6 Generator ---------------
+
 function* foo_es6(url) {
+  // STATE 1
+  // Initial state
   try {
     console.log('Requesting: ', url);
-    const value = yield fetch(url);
+    const tmp1 = fetch(url);
+
+    // STATE 2
+    // Success state
+    const value = yield tmp1;
     console.log('Value: ', value);
   } catch (error) {
+    // STATE 3
+    // Failure state
     console.log('Oops: ', error);
     return false;
   }
@@ -12,3 +22,22 @@ function* foo_es6(url) {
 const it = foo_es6('https://jsonplaceholder.typicode.com/todos');
 console.log(it.next());
 console.log(it.next());
+// ---------------------------------------------
+
+
+// ------------ Pre-ES6 Generator --------------
+
+// Function needs to return a generator
+function foo_pre(url) {
+
+  // Make and return a generator
+  return {
+    next: function (value) { },
+    throw: function (error) { }
+  }
+}
+
+const it = foo_pre('https://jsonplaceholder.typicode.com/todos');
+console.log(it.next());
+console.log(it.next());
+// ---------------------------------------------
