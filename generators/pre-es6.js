8c106304ce64d2eb9e28dc9171f5da88e82464cb
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-24 20:52:23 +0300

    Add a generator code

---
+++ generators/pre-es6.js
@@ -0,0 +1,14 @@
+function *foo_es6(url) {
+  try {
+    console.log("Requesting: ", url);
+    const value = yield fetch(url);
+    console.log("Value: ", value);
+  } catch (error) {
+    console.log("Oops: ", error);
+    return false;
+  }
+}
+
+const it = foo_es6('https://jsonplaceholder.typicode.com/todos');
+console.log(it.next());
+console.log(it.next());
