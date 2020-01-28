f0d14f42cde635f7352b77c53084d8aba3e60825
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-28 21:52:03 +0300

    Add next and throw iterator methods

--- generators/pre-es6.js
+++ generators/pre-es6.js
@@ -19,11 +19,12 @@ function* foo_es6(url) {
   }
 }
 
-const it = foo_es6('https://jsonplaceholder.typicode.com/todos');
-console.log(it.next());
-console.log(it.next());
+console.log('-------- ES6 Generator ---------');
+const it_es6 = foo_es6('https://jsonplaceholder.typicode.com/todos');
+const value_es6 = it_es6.next();
+console.log('Final: ', it_es6.next(value_es6));
+console.log('--------------------------------');
 // ---------------------------------------------
-
 
 // ------------ Pre-ES6 Generator --------------
 
@@ -36,17 +37,17 @@ function foo_pre(url) {
   var sharedValue;
 
   // Handle each state
-  function process(value) {
+  function handleState(value) {
     switch (state) {
       case 1:
         // STATE 1
         // Initial state
         console.log('Requesting: ', url);
-        return fetch(url);    
+        return fetch(url);
       case 2:
         // STATE 2
         // Success state
-        sharedValue = value
+        sharedValue = value;
         console.log('Value: ', sharedValue);
         return;
       case 3:
@@ -60,12 +61,51 @@ function foo_pre(url) {
 
   // Make and return a generator
   return {
-    next: function (value) { },
-    throw: function (error) { }
+    next: function (value) {
+      if (!state) {
+        // STATE 1
+        // Initial state
+        state = 1;
+        return {
+          done: false,
+          value: handleState()
+        }
+      } else if (state === 1) {
+        // STATE 2
+        // Success state
+        state = 2;
+        return {
+          done: true,
+          value: handleState(value)
+        }
+      };
+
+      // Generator completed
+      return {
+        done: true,
+        value: undefined
+      }
+    },
+    throw: function (error) {
+      if (state === 1) {
+        // STATE 3
+        // Failure state
+        state = 3;
+        return {
+          done: true,
+          value: handleState(error)
+        }
+      }
+
+      // Case for error NOT during the request
+      throw error;
+    }
   }
 }
 
-const it = foo_pre('https://jsonplaceholder.typicode.com/todos');
-console.log(it.next());
-console.log(it.next());
+console.log('------- Pre-ES6 Generator ------');
+const it_pre = foo_pre('https://jsonplaceholder.typicode.com/todos');
+const value_pre = it_pre.next();
+console.log('Final: ', it_pre.next(value_pre));
+console.log('--------------------------------');
 // ---------------------------------------------
