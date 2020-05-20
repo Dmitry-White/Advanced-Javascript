5952a4cd751c7c884f11f3f835dba4b5a6090eda
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-20 12:00:23 +0300

    Add naive example with await

---
+++ async-iife/naive.js
@@ -0,0 +1,33 @@
+/*
+  The problem here is in the blocking execution 
+  (in terms of "one operation after another").
+  E.g we don't really need "duration" to start importing "album",
+  we could be doing it concurrently.
+*/
+
+const importHandler = async (file) => {
+  // Read the file
+  const buffer = await read(file);
+
+  // Parse out the ID3 metadata
+  const meta = await parser(file);
+  const songMeta = mapSongMeta(meta);
+  const albumMeta = mapAlbumMeta(meta);
+
+  // Compute the duration
+  const duration = await getDuration(buffer);
+
+  // Import the album
+  const albumId = await importAlbum(albumMeta);
+
+  // Import the song
+  const songId = await importSong({
+    ...songMeta,
+    albumId,
+    file,
+    duration,
+    meta
+  });
+
+  return songId;
+}
