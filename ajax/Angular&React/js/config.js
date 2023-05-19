3625561edb66644d20eb480203bd8b66c72daf42
Dmitry White <dmitry.b.white@gmail.com>
2023-05-19 14:10:52 +0100

    Fix AJAX section

---
+++ ajax/Angular&React/js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  WEATHER_API: {
+    URL: 'http://api.openweathermap.org/data/2.5/weather',
+    KEY: '<API_KEY>',
+  },
+  EVENTBRITE_API: {
+    URL: 'https://www.eventbriteapi.com/v3/',
+    KEY: '<API_KEY>',
+  },
+};
