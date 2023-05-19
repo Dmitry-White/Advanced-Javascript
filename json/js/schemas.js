8e5e674fa4f576b6eb5cf22fb4c3e7a0a643948a
Dmitry White <dmitry.b.white@gmail.com>
2023-05-19 14:11:31 +0100

    Fix JSON section

--- json/js/schemas.js
+++ json/js/schemas.js
@@ -1,94 +1,76 @@
 const productSchema = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "id": "http://example.com/schemas/products.json",
-  "title": "H+ sport products",
-  "description": "Schema for H+ sport product data",
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  id: 'http://example.com/schemas/products.json',
+  title: 'H+ sport products',
+  description: 'Schema for H+ sport product data',
 
-  "type:": "object",
-  "properties": {
-    "products": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string"
+  'type:': 'object',
+  properties: {
+    products: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
           },
-          "image": {
-            "type": "string"
+          image: {
+            type: 'string',
           },
-          "alt": {
-            "type": "string"
-          }
+          alt: {
+            type: 'string',
+          },
         },
-        "required": [
-          "name",
-          "image",
-          "alt"
-        ]
-      }
-    }
+        required: ['name', 'image', 'alt'],
+      },
+    },
   },
-  "required": [
-    "products"
-  ]
+  required: ['products'],
 };
 
 const personSchema = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "id": "http://example.com/schemas/people.json",
-  "title": "h+ sport people",
-  "description": "schema for h+ sport person cards",
-  "type": "object",
-  "properties": {
-    "cards": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "img": {
-            "type": "object",
-            "properties": {
-              "src": {
-                "type": "string"
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  id: 'http://example.com/schemas/people.json',
+  title: 'h+ sport people',
+  description: 'schema for h+ sport person cards',
+  type: 'object',
+  properties: {
+    cards: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          img: {
+            type: 'object',
+            properties: {
+              src: {
+                type: 'string',
               },
-              "alt": {
-                "type": "string"
-              }
-            },
-            "required": [
-              "src",
-              "alt"
-            ]
-          },
-          "cardInfo": {
-            "type": "object",
-            "properties": {
-              "cardName": {
-                "type": "string"
+              alt: {
+                type: 'string',
               },
-              "cardTitle": {
-                "type": "string"
-              }
             },
-            "required": [
-              "cardName",
-              "cardTitle"
-            ]
+            required: ['src', 'alt'],
           },
-          "cardText": {
-            "type": "string"
-          }
+          cardInfo: {
+            type: 'object',
+            properties: {
+              cardName: {
+                type: 'string',
+              },
+              cardTitle: {
+                type: 'string',
+              },
+            },
+            required: ['cardName', 'cardTitle'],
+          },
+          cardText: {
+            type: 'string',
+          },
         },
-        "required": [
-          "img",
-          "cardInfo",
-          "cardText"
-        ]
-      }
-    }
+        required: ['img', 'cardInfo', 'cardText'],
+      },
+    },
   },
-  "required": [
-    "cards"
-  ]
+  required: ['cards'],
 };
--- json/js/script.js
+++ json/js/script.js
@@ -22,9 +22,9 @@ const App = () => {
 
   const displayProductsXML = (data) => {
     const j2x = json2xml(data);
-    const section = document.querySelector(".product-list");
+    const section = document.querySelector('.product-list');
     section.innerHTML = j2x;
-  }
+  };
 
   const displayPeopleXML = (data) => {
     const parser = new DOMParser();
@@ -34,21 +34,21 @@ const App = () => {
 
     const fragment = document.createDocumentFragment();
 
-    const section = document.querySelector(".people-cards");
-    const item = document.createElement("div");
-    const image = document.createElement("img");
-    const cardInfo = document.createElement("div");
-    const cardName = document.createElement("h3");
-    const cardTitle = document.createElement("h4");
-    const cardText = document.createElement("p");
+    const section = document.querySelector('.people-cards');
+    const item = document.createElement('div');
+    const image = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const cardName = document.createElement('h3');
+    const cardTitle = document.createElement('h4');
+    const cardText = document.createElement('p');
 
     let person = EMPTY_OBJECT;
 
-    item.className = "person-card";
-    cardInfo.className = "card-info";
-    cardName.className = "card-name";
-    cardTitle.className = "card-title";
-    cardText.className = "card-text";
+    item.className = 'person-card';
+    cardInfo.className = 'card-info';
+    cardName.className = 'card-name';
+    cardTitle.className = 'card-title';
+    cardText.className = 'card-text';
 
     cardInfo.appendChild(cardName);
     cardInfo.appendChild(cardTitle);
@@ -59,13 +59,13 @@ const App = () => {
     parsedData.people.div.forEach((el) => {
       person = item.cloneNode(true);
 
-      const currentImage = person.querySelector("img");
-      currentImage.src = el.img["@src"];
-      currentImage.alt = el.img["@alt"];
+      const currentImage = person.querySelector('img');
+      currentImage.src = el.img['@src'];
+      currentImage.alt = el.img['@alt'];
 
-      person.querySelector(".card-name").innerHTML = el.div.h3["#text"];
-      person.querySelector(".card-title").innerHTML = el.div.h4["#text"];
-      person.querySelector(".card-text").innerHTML = el.p["#text"];
+      person.querySelector('.card-name').innerHTML = el.div.h3['#text'];
+      person.querySelector('.card-title').innerHTML = el.div.h4['#text'];
+      person.querySelector('.card-text').innerHTML = el.p['#text'];
 
       fragment.appendChild(person);
     });
@@ -75,17 +75,17 @@ const App = () => {
   const displayProducts = (data) => {
     // create document fragment and template for each product
     const fragment = document.createDocumentFragment();
-    
-    const section = document.querySelector(".product-list");
-    const item = document.createElement("li");
-    const image = document.createElement("img");
-    const text = document.createElement("h2");
+
+    const section = document.querySelector('.product-list');
+    const item = document.createElement('li');
+    const image = document.createElement('img');
+    const text = document.createElement('h2');
 
     let product = EMPTY_OBJECT;
 
-    item.className = "product-item";
-    image.className = "product-image";
-    text.className = "product-name";
+    item.className = 'product-item';
+    image.className = 'product-image';
+    text.className = 'product-name';
 
     item.appendChild(image);
     item.appendChild(text);
@@ -94,37 +94,37 @@ const App = () => {
     data.products.forEach((el) => {
       product = item.cloneNode(true);
 
-      const currentImage = product.querySelector(".product-image");
-      currentImage.src = "images/products/" + el.image;
+      const currentImage = product.querySelector('.product-image');
+      currentImage.src = `images/products/${el.image}`;
       currentImage.alt = el.alt;
 
-      product.querySelector(".product-name").innerHTML = el.name;
+      product.querySelector('.product-name').innerHTML = el.name;
 
       fragment.appendChild(product);
     });
 
     // insert the document fragment at the appropriate place in the document
     section.appendChild(fragment);
-  }
+  };
 
   const displayPeople = (data) => {
     const fragment = document.createDocumentFragment();
 
-    const section = document.querySelector(".people-cards");
-    const item = document.createElement("div");
-    const image = document.createElement("img");
-    const cardInfo = document.createElement("div");
-    const cardName = document.createElement("h3");
-    const cardTitle = document.createElement("h4");
-    const cardText = document.createElement("p");
+    const section = document.querySelector('.people-cards');
+    const item = document.createElement('div');
+    const image = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const cardName = document.createElement('h3');
+    const cardTitle = document.createElement('h4');
+    const cardText = document.createElement('p');
 
     let person = EMPTY_OBJECT;
 
-    item.className = "person-card";
-    cardInfo.className = "card-info";
-    cardName.className = "card-name";
-    cardTitle.className = "card-title";
-    cardText.className = "card-text";
+    item.className = 'person-card';
+    cardInfo.className = 'card-info';
+    cardName.className = 'card-name';
+    cardTitle.className = 'card-title';
+    cardText.className = 'card-text';
 
     cardInfo.appendChild(cardName);
     cardInfo.appendChild(cardTitle);
@@ -136,40 +136,40 @@ const App = () => {
     data.cards.forEach((el) => {
       person = item.cloneNode(true);
 
-      const currentImage = person.querySelector("img");
-      currentImage.src = "images/employees/" + el.img.src;
+      const currentImage = person.querySelector('img');
+      currentImage.src = `images/employees/${el.img.src}`;
       currentImage.alt = el.img.alt;
 
-      person.querySelector(".card-name").innerHTML = el.cardInfo.cardName;
-      person.querySelector(".card-title").innerHTML = el.cardInfo.cardTitle;
-      person.querySelector(".card-text").innerHTML = el.cardText;
+      person.querySelector('.card-name').innerHTML = el.cardInfo.cardName;
+      person.querySelector('.card-title').innerHTML = el.cardInfo.cardTitle;
+      person.querySelector('.card-text').innerHTML = el.cardText;
 
       fragment.appendChild(person);
     });
 
     // insert the document fragment at the appropriate place in the document
     section.appendChild(fragment);
-  }
+  };
 
   const validate = (data, schema, callback) => {
     const dataCheck = tv4.validateMultiple(data, schema);
-    
+
     if (dataCheck.valid) {
       console.log('Valid Data');
       callback(data);
     } else {
       console.log(dataCheck);
     }
-  }
+  };
 
   displayProductsXML(PRODUCT_DATA_XML);
   displayPeopleXML(PERSON_DATA_XML);
 
   displayProductsYAML(PRODUCT_DATA);
-  displayPeopleYAML(PERSON_DATA);  
+  displayPeopleYAML(PERSON_DATA);
 
   validate(PRODUCT_DATA, productSchema, displayProducts);
   validate(PERSON_DATA, personSchema, displayPeople);
-}
+};
 
-document.addEventListener("DOMContentLoaded", App);
+document.addEventListener('DOMContentLoaded', App);
