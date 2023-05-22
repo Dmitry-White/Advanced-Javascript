b6c090cfcc1bc67c86db150f286f817663afa111
Dmitry White <dmitry.b.white@gmail.com>
2023-05-22 15:47:10 +0100

    Refactor Advanced Generators

--- advanced/generators/pre-es6.js
+++ advanced/generators/pre-es6.js
@@ -1,34 +1,3 @@
-// --------------- ES6 Generator ---------------
-
-function* generateDataES6(url) {
-  // STATE 1
-  // Initial state
-  try {
-    console.log('Requesting: ', url);
-    const tmp1 = fetch(url);
-
-    // STATE 2
-    // Success state
-    const value = yield tmp1;
-    console.log('Value: ', value);
-    return null;
-  } catch (error) {
-    // STATE 3
-    // Failure state
-    console.log('Oops: ', error);
-    return error;
-  }
-}
-
-console.log('-------- ES6 Generator ---------');
-const itES6 = generateDataES6('https://jsonplaceholder.typicode.com/todos');
-const valueES6 = itES6.next();
-console.log('Final: ', itES6.next(valueES6));
-console.log('--------------------------------');
-// ---------------------------------------------
-
-// ------------ Pre-ES6 Generator --------------
-
 // Function needs to return a generator
 function generateDataPre(url) {
   // Generator state
@@ -115,4 +84,3 @@ const itPre = generateDataPre('https://jsonplaceholder.typicode.com/todos');
 const valuePre = itPre.next();
 console.log('Final: ', itPre.next(valuePre));
 console.log('--------------------------------');
-// ---------------------------------------------
