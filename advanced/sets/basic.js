97455c4fe7f830cd4157cae11f4617a4d90ffa93
Dmitry White <dmitry.b.white@gmail.com>
2023-05-23 16:13:37 +0100

    Add Advanced Sets basic setup

---
+++ advanced/sets/basic.js
@@ -0,0 +1,32 @@
+/*
+  The Set object lets you store unique values and remembers the original insertion order.
+  Any primitive values or object references may be used as a value.
+  A value in the set may only occur once;
+*/
+
+// Letters Guessed
+// You are playing a game of snowman with a friend and keep track of
+// letters that have been guessed using Set().
+// Follow the tasks below to test your knowledge.
+
+// 1. So far, your friend has gussed the following letters: r, s, t, l, n, e.
+// Create a Set using the letters guessed thus far.
+
+// 2. Your friend takes another guess: p. Before you add it to the set check
+// to see if the Set has 'p'. If not, add it to the Set.
+
+// 3. At this point, we will assume your friend hasn't tried to guess the same letter twice.
+// The size of the Set will indicate how many guesses he's made. How many guesses
+// has your friend made?
+
+// 4. Delete the letter 'l' from your Set.
+
+// 5. What is the second value in your Set?
+
+// 6. What is the first entry in your Set?
+
+// 7. For each entry in your Set, log out whether the guess was a consonant or a vowel.
+// This one is a little challenging!
+// const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// 8. Clear your Set!
